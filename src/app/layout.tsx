d5d56e99ee16cbb27c299909d45cf88c0252fbbd
Jelilat Anofiu <anofiujelailah@gmail.com>
2024-01-15 08:10:12 +0100

    add opengraph to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,26 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
 import { Exo } from "next/font/google";
 const inter = Exo({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Starknet JSON-RPC Request Builder",
-  description: `An interactive web tool designed for developers working with Starknet. This app simplifies the process of creating and testing JSON-RPC requests on Starknet. Users can easily construct, modify, and send requests using Nethermind's free RPC Service.`,
+  description: `An interactive web tool designed for developers working with Starknet. This app simplifies the process of creating and testing JSON-RPC requests. Users can easily construct, modify, and send requests using Nethermind's free RPC Service.`,
+  openGraph: {
+    type: "website",
+    locale: "en_UK",
+    url: "https://rpc-request-builder.voyager.online/",
+    siteName: "Starknet JSON-RPC Request Builder",
+    images: [
+      {
+        url: "https://rpc-request-builder.voyager.online/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Starknet JSON-RPC Request Builder",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
